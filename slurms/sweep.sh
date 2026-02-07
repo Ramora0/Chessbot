2c4a770ba88d0b0bc732a88c17be4137bb3e3683
Ramora0 <99ilovecats99@gmail.com>
2026-02-07 13:56:32 -0500

    sweep

--- slurms/sweep.sh
+++ slurms/sweep.sh
@@ -1,3 +1,4 @@
-sbatch slurms/a100.slurm master wide-shallow --ffn-dim 2048 --depth 12
-sbatch slurms/a100.slurm master more-heads --heads 24
+sbatch slurms/a100.slurm master wide-shallow --ffn-dim 1536 --depth 14
+sbatch slurms/a100.slurm master more-heads --heads 16
 sbatch slurms/a100.slurm master beta2 --beta2 0.98
+sbatch slurms/a100.slurm master no-reg --weight-decay 0 --dropout 0
