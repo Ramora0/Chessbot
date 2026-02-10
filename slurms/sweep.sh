b9437e54d31e18ea801f8d5ded4029db0f974c5d
Ramora0 <99ilovecats99@gmail.com>
2026-02-10 10:01:26 -0500

    sweep

--- slurms/sweep.sh
+++ slurms/sweep.sh
@@ -1,3 +1,3 @@
-sbatch slurms/h100.slurm dynamic-temp dynamic-temp
-sbatch slurms/h100.slurm master only-softmax --mate-logit-bonus 10
-sbatch slurms/h100.slurm master mates-4p --mate-bucket 0.04
+sbatch slurms/a100.slurm dynamic-temp dynamic-temp --grad-accum 2
+sbatch slurms/a100.slurm master only-softmax --mate-logit-bonus 10 --grad-accum 2
+sbatch slurms/a100.slurm master mates-4p --mate-bucket 0.04 --grad-accum 2
